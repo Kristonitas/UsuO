9735a57b6767e8969580ec8250e85b58264fbf18
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2016-03-10 13:53:33 +0200

    Initial commit with active admin devise and questions

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :confirmable, :registerable,
+         :recoverable, :rememberable, :trackable, :timeoutable, :validatable
+
+  has_many :questions, dependent: :destroy
+
+end
