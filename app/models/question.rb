c859843bc7744b8a305c4ec14b5242bef890ad2c
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2016-03-23 12:26:30 +0200

    comments being displayed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,15 @@
 class Question < ActiveRecord::Base
   has_secure_token :id_token
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
   validates :user_id, presence: true
   validates :title, presence: true
 
-  has_many :answers, class_name: "Response"
+  has_many :responses
+
+  def answers
+    responses.where(response_id: nil)
+  end
   
   def to_param
     id_token
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,14 +1,14 @@
 class Response < ActiveRecord::Base
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(created_at: :asc) }
   
   belongs_to :response
   belongs_to :question
   belongs_to :user
+  has_many :comments, class_name: "Response"
 
   validates :user_id, presence: true
   validates :question_id, presence: true
   validates :content, presence: true
   
   has_secure_token :id_token
-  has_many :comments, class_name: "Response"
 end
